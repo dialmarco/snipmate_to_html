23d87c950a67dcf806a1e87f3ca258ae15ce8923
Diego Alonso <diego@diegoalonso.net>
2010-12-31 07:58:18 +1100

    snippets in table

--- snipmate_to_html.rb
+++ snipmate_to_html.rb
@@ -7,11 +7,33 @@ class Snippet
   attr_accessor :file
 end
 directory = ARGV[0]
+snippets = {}
 Find.find(directory) do |file| 
   if file =~ /snippet$/
     snippet = Snippet.new
     snippet.file = file
-    snippet.module, snippet.snippet = file.gsub(directory, "").split /\//
-    puts "#{snippet.module} : #{snippet.snippet} => #{snippet.file}"
+    splitted = file.gsub(directory, "").split /\//
+    snippet.module = splitted[0..-2]
+    snippet.snippet = splitted[-1]
+    snippets[snippet.module] ||= []
+    snippets[snippet.module] << snippet
   end
+end
+
+snippets.each do |key, value|
+  print "<h1>"
+  key.each do |subkey|
+    print "#{subkey}:"
+  end
+  puts "</h1>"
+  puts "<table>"
+  value.each do |snippet|
+    puts "<tr>"
+    print "<td>#{snippet.snippet}</td><td>"
+    File.open(snippet.file).each_line do |line|
+      print "#{line}<br />"
+    end
+    puts "</td></tr>"
+  end
+  puts "</table>"
 end
